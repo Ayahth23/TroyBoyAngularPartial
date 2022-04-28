255105e25b6efc710a7cd68f6598d4ce888824e2
Ayahth23 <tharwa@rpi.edu>
2022-04-27 23:31:19 -0400

    chat

---
+++ angular-marketplace-final/src/CONSTS.ts
@@ -0,0 +1,23 @@
+export const COMETCHAT_CONSTANTS = {
+  // TODO: insert APP_ID, REGION and AUTH_KEY
+  APP_ID: "2079734accfef242",
+  REGION: "us",
+  AUTH_KEY: "66a6c242484b8560eda618b113d007290dfd4839",
+  UIDs: {
+    divya: "user1",
+    jialin: "user2",
+    chloe: "user3",
+    ayah: "user4",
+
+  },
+  imgUrls: {
+    divya: "assets/divya.jpeg",
+    jialin:
+      "assets/jialin.jpeg",
+    chloe:
+      "assets/chloe.jpeg",
+    ayah:
+      "assets/ayah.jpeg",
+  },
+};
+
